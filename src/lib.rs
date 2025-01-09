0585cf17a08c12fc06e32d847b17c0b1725b1a31
Ron Unrau <rcunrau@ualberta.ca>
2025-01-08 22:06:43 -0700

    Move uart.rs back into lib.rs and add Spi as a second transport type

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,70 @@
 use std::io;
-
-pub mod uart;
+use std::io::{Read, Write};
+use std::os::fd::{AsFd, AsRawFd};
+use nix::unistd::{read, write};
+use std::fs::{File, OpenOptions};
+use spidev::Spidev;
 
 pub trait SendRecv {
-    fn send(&self, data: &[u8]) -> io::Result<usize>;
+    fn send(&mut self, data: &[u8]) -> io::Result<usize>;
 
-    fn recv(&self, data: &mut [u8]) -> io::Result<usize>;
+    fn recv(&mut self, data: &mut [u8]) -> io::Result<usize>;
+}
+
+pub struct Uart {
+    path: String,
+    fd: File,
+}
+
+impl Uart {
+    pub fn open(devpath: &String) -> io::Result<Uart> {
+        let fd = OpenOptions::new().read(true).write(true).open(devpath)?;
+        Ok(Uart {
+            path: devpath.clone(),
+            fd,
+        })
+    }
+
+    pub fn name(&self) -> &String {
+        &self.path
+    }
+}
+      
+impl SendRecv for Uart {
+    fn send(&mut self, data: &[u8]) -> io::Result<usize> {
+        Ok(write(self.fd.as_fd(), data)?)
+    }
+
+    fn recv(&mut self, data: &mut [u8]) -> io::Result<usize> {
+        Ok(read(self.fd.as_raw_fd(), data)?)
+    }
+}
+
+pub struct Spi {
+    path: String,
+    device: Spidev,
+}
+
+impl Spi {
+    pub fn open(devpath: &String) -> io::Result<Spi> {
+        let device = Spidev::open(devpath)?;
+        Ok(Spi {
+            path: devpath.clone(),
+            device,
+        })
+    }
+
+    pub fn name(&self) -> &String {
+        &self.path
+    }
+}
+      
+impl SendRecv for Spi {
+    fn send(&mut self, data: &[u8]) -> io::Result<usize> {
+        Ok(self.device.write(data)?)
+    }
+
+    fn recv(&mut self, data: &mut [u8]) -> io::Result<usize> {
+        Ok(self.device.read(data)?)
+    }
 }
